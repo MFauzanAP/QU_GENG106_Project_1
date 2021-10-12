81e7c62bf493f552cfe5e504b985bb604b9d3286
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-10-12 12:43:10 +0300

    Update graphics.py to clean up property names for output

--- utilities.py
+++ utilities.py
@@ -1,2 +1,11 @@
 # Function used to check if a value is above 0
 is_above_0 = lambda x: x > 0
+
+# Function used to convert from property to string
+def property_to_string(property):
+
+	# Remove all underscores and capitalise the start of each word
+	output = property.replace('_', ' ').title()
+
+	# Return output string
+	return output
